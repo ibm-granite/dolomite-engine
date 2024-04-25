377de0827e1c2daf4e06c386093e7ec7ffd5c0e7
Mayank Mishra <mayank.mishra2@ibm.com>
2024-04-25 17:03:53 -0400

    deep mla

--- dolomite_engine/hf_models/models/gpt_multilayer/attention/base.py
+++ dolomite_engine/hf_models/models/gpt_multilayer/attention/base.py
@@ -116,10 +116,8 @@ class KeyValueProjection(nn.Module):
             hidden_states = self.ln(hidden_states)
             hidden_states = self.kv_attn(hidden_states)
         else:
-            residual = hidden_states
             for l in self.kv_attn:
                 hidden_states = l(hidden_states)
-            hidden_states = hidden_states + residual
 
         if self.num_key_value_heads == 1:
             hidden_states = hidden_states.unsqueeze(1)

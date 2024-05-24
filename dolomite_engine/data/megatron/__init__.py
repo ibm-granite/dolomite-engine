f476c4b1d51a5dcd9b9a0720586a29d361a3834b
Mayank Mishra <mayank31398@gmail.com>
2024-05-23 21:06:34 -0400

    drop print_rank_0

--- dolomite_engine/data/megatron/__init__.py
+++ dolomite_engine/data/megatron/__init__.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer
 
 from ...arguments import TrainingArgs
 from ...defaults import INPUT_FORMAT, OUTPUT_FORMAT
-from ...utils import get_global_rank, get_world_size, log_rank_0
+from ...utils import get_world_size, log_rank_0
 from ..dataloader import ResumableDataLoader
 from .blended_megatron_dataset_builder import BlendedMegatronDatasetBuilder
 from .blended_megatron_dataset_config import GPTDatasetConfig

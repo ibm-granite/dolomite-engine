f300ca0424774949087ff513ca001e42073d22db
Mayank Mishra <mayank31398@gmail.com>
2024-06-18 18:23:49 -0400

    dict

--- dolomite_engine/hf_models/modeling_utils_TP/normalization.py
+++ dolomite_engine/hf_models/modeling_utils_TP/normalization.py
@@ -1,4 +1,5 @@
 from functools import partial
+from typing import Any, Mapping
 
 import torch.nn as nn
 from torch.distributed._tensor.api import DTensor
@@ -6,7 +7,11 @@ from torch.distributed._tensor.placement_types import Replicate
 
 from ...utils import ProcessGroupManager
 from ..modeling_utils import get_normalization_function
-from .TP import prepare_tensor_parallel_dtensor_input, prepare_tensor_parallel_tensor_output
+from .TP import (
+    modify_state_dict_to_densor_dict,
+    prepare_tensor_parallel_dtensor_input,
+    prepare_tensor_parallel_tensor_output,
+)
 
 
 def get_normalization_function_TP(
@@ -31,5 +36,13 @@ def get_normalization_function_TP(
     normalization_function.register_forward_hook(
         partial(prepare_tensor_parallel_tensor_output, assert_placement=Replicate())
     )
+
+    original_load_state_dict = normalization_function.load_state_dict
+
+    def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False) -> None:
+        state_dict = modify_state_dict_to_densor_dict(self, state_dict)
+        return original_load_state_dict(state_dict, strict, assign)
+
+    normalization_function.load_state_dict = load_state_dict
 
     return normalization_function

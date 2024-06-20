ea05c58ca62d026b9a7566ac6207ceb4c9b607dd
Mayank Mishra <mayank31398@gmail.com>
2024-06-20 19:50:04 -0400

    dcp test

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
@@ -143,13 +143,13 @@ class GPTDolomiteForCausalLM_TP(GPTDolomitePreTrainedModel_TP, GPTDolomiteForCau
     @classmethod
     def from_pretrained(
         self,
-        model_name: str,
+        pretrained_model_name_or_path: str,
         torch_dtype: torch.dtype = torch.float32,
         tensor_parallel_embeddings: bool = False,
     ) -> GPTDolomiteForCausalLM_TP:
         # use dummy tensors to avoid initializing model here
         with torch.device("meta"):
-            config = GPTDolomiteConfig.from_pretrained(model_name)
+            config = GPTDolomiteConfig.from_pretrained(pretrained_model_name_or_path)
             # try sharding vocab matrices if really struggling for memory
             model = GPTDolomiteForCausalLM_TP(config, tensor_parallel_embeddings=tensor_parallel_embeddings)
 
@@ -161,7 +161,7 @@ class GPTDolomiteForCausalLM_TP(GPTDolomitePreTrainedModel_TP, GPTDolomiteForCau
 
         # load weights into tensor parallel model using SafeTensorsWeightsManager class
         # this avoids loading multiple copies of the parameters in CPU memory
-        safetensors_weight_manager = SafeTensorsWeightsManager(model_name)
+        safetensors_weight_manager = SafeTensorsWeightsManager(pretrained_model_name_or_path)
         model.load_from_safetensors_weights_manager(safetensors_weight_manager)
 
         return model

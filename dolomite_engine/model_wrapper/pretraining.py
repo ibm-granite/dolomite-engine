0cd822a3798e4fbf36d35be9ffb3554a685fe071
Mayank Mishra <mayank31398@gmail.com>
2024-07-04 17:32:11 -0400

    tp loss

--- dolomite_engine/model_wrapper/pretraining.py
+++ dolomite_engine/model_wrapper/pretraining.py
@@ -1,13 +1,16 @@
+import contextlib
 from typing import Union
 
 import torch
 import torch.distributed
 import torch.nn.functional as F
+from torch.distributed._tensor.api import DTensor
+from torch.distributed._tensor.placement_types import Shard
+from torch.distributed.tensor.parallel import loss_parallel
 
 from dolomite_engine.enums import Mode
 
 from ..arguments import InferenceArgs, TrainingArgs, UnshardingArgs
-from ..hf_models.modeling_utils_TP import TensorParallelCrossEntropy
 from ..utils import ProcessGroupManager
 from .base import ModelWrapper
 
@@ -40,6 +43,7 @@ class ModelWrapperForPretraining(ModelWrapper):
         if self.tp_world_size > 1:
             tp_source_rank = ProcessGroupManager.get_tensor_parallel_first_rank()
             tp_group = ProcessGroupManager.get_tensor_parallel_group()
+            tp_mesh = ProcessGroupManager.get_tensor_parallel_mesh()
 
             if self.tp_rank == 0:
                 tokens: torch.Tensor = batch["text"]
@@ -57,15 +61,18 @@ class ModelWrapperForPretraining(ModelWrapper):
             input_ids = tokens[:, :-1]
             labels = tokens[:, 1:]
 
+            model_outputs = self.model(
+                input_ids=input_ids, output_parallel_lm_logits=self.tensor_parallel_word_embeddings
+            )
+            logits = model_outputs[0] if isinstance(model_outputs, tuple) else model_outputs.logits
+
             if self.tensor_parallel_word_embeddings:
-                model_outputs = self.model(input_ids=input_ids, output_parallel_lm_logits=True)
+                assert not self.upcast_logits_for_loss
+                logits = DTensor.from_local(logits, device_mesh=tp_mesh, placements=[Shard(-1)])
 
-                logits = model_outputs[0] if isinstance(model_outputs, tuple) else model_outputs.logits
-                loss = TensorParallelCrossEntropy.apply(logits, labels, self.vocab_size, self.upcast_logits_for_loss)
+                with loss_parallel():
+                    loss = F.cross_entropy(logits.view(-1, logits.size(-1)), labels.reshape(-1))
             else:
-                model_outputs = self.model(input_ids=input_ids)
-
-                logits = model_outputs[0] if isinstance(model_outputs, tuple) else model_outputs.logits
                 if self.upcast_logits_for_loss:
                     logits = logits.float()
                 loss = F.cross_entropy(logits.view(-1, logits.size(-1)), labels.reshape(-1))

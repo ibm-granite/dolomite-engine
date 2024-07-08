300606e193f4de1cdbb585f3db43aac8a9138a04
Mayank Mishra <mayank31398@gmail.com>
2024-07-08 18:43:12 -0400

    dtensors

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
@@ -4,7 +4,6 @@ from typing import Tuple, Union
 
 import torch
 import torch.nn.functional as F
-from torch.distributed._tensor.api import DTensor
 from torch.distributed._tensor.placement_types import Replicate, Shard
 from transformers import DynamicCache
 from transformers.modeling_outputs import CausalLMOutputWithPast
@@ -12,9 +11,10 @@ from transformers.modeling_outputs import CausalLMOutputWithPast
 from ....utils import ProcessGroupManager, SafeTensorsWeightsManager
 from ...modeling_utils_TP import (
     LMHead_TP,
-    copy_to_tensor_parallel_region,
+    dtensor_to_tensor,
     gather_from_tensor_parallel_region,
     tensor_parallel_cross_entropy,
+    tensor_to_dtensor,
 )
 from ..gpt_dolomite import GPTDolomiteConfig, GPTDolomiteForCausalLM, GPTDolomitePreTrainedModel
 from .base import GPTDolomiteModel_TP, GPTDolomitePreTrainedModel_TP
@@ -104,14 +104,12 @@ class GPTDolomiteForCausalLM_TP(GPTDolomitePreTrainedModel_TP, GPTDolomiteForCau
         )
 
     def get_lm_logits(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        if self.tensor_parallel_word_embeddings:
-            hidden_states = copy_to_tensor_parallel_region(hidden_states)
-
-        return (
-            F.linear(hidden_states, self.transformer.wte.weight.to_local())
-            if self._tied_word_embeddings
-            else self.lm_head(hidden_states)
+        hidden_states = tensor_to_dtensor(hidden_states, current_placement=Replicate())
+        hidden_states = super().get_lm_logits(hidden_states)
+        hidden_states = dtensor_to_tensor(
+            hidden_states, desired_placement=Shard(-1) if self.tensor_parallel_word_embeddings else Replicate()
         )
+        return hidden_states
 
     def get_autoregressive_language_modeling_loss(self, lm_logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
         if labels is None:

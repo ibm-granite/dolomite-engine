91e170166062e7483efa37a0ae4521714524b461
Mayank Mishra <mayank31398@gmail.com>
2024-07-08 17:27:13 -0400

    revert

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/main.py
@@ -5,8 +5,6 @@ from typing import Tuple, Union
 
 import torch
 import torch.nn.functional as F
-from torch.distributed._tensor.api import DTensor
-from torch.distributed._tensor.placement_types import Replicate, Shard
 from torch.distributed.tensor.parallel import loss_parallel
 from transformers import DynamicCache
 from transformers.modeling_outputs import CausalLMOutputWithPast

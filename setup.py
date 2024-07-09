7a4ed385ceb9086d90b84f631be464f9c6069a8e
Mayank Mishra <mayank31398@gmail.com>
2024-07-09 17:43:15 -0400

    cleaner dependencies

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 from setuptools import find_packages, setup
 
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-    install_reqs = parse_requirements('requirements.txt', session='packaging')
-    reqs = [str(ir.requirement) for ir in install_reqs]
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    install_reqs = parse_requirements('requirements.txt', session='packaging')
-    reqs = [str(ir.req) for ir in install_reqs]
-
+with open("requirements.txt") as requirements:
+    requirements = requirements.readlines()
+    requirements = [i.strip() for i in requirements]
 
 VERSION = "0.0.1.dev"
 
 setup(
     name="dolomite-engine",
     version=VERSION,
-    install_requires=reqs,
+    install_requires=requirements,
     author="Mayank Mishra",
     author_email="mayank.mishra2@ibm.com",
+    license="Apache License 2.0",
     url="https://github.com/ibm-granite/dolomite-engine",
-    packages=find_packages("./"),
+    packages=find_packages("./", exclude="tests/"),
     include_package_data=True,
     package_data={"": ["**/*.cu", "**/*.cpp", "**/*.cuh", "**/*.h", "**/*.pyx"]},
 )

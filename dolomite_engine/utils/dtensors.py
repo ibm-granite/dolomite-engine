dccce07732ecefef8a895d2a05e333621d7b3be4
Mayank Mishra <mayank31398@gmail.com>
2024-07-08 23:21:02 -0400

    contextmanager

--- dolomite_engine/utils/dtensors.py
+++ dolomite_engine/utils/dtensors.py
@@ -5,11 +5,11 @@ _USE_DTENSORS_FOR_COMPUTATIONS: bool = False
 
 
 @contextmanager
-def enable_dtensors_for_computation():
+def enable_dtensors_for_computation(enable: bool = True):
     global _USE_DTENSORS_FOR_COMPUTATIONS
 
     original_value = _USE_DTENSORS_FOR_COMPUTATIONS
-    _USE_DTENSORS_FOR_COMPUTATIONS = True
+    _USE_DTENSORS_FOR_COMPUTATIONS = enable
 
     yield
 

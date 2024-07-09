b708abf64225bdec8a55ddf39b2611bc4980df1f
Mayank Mishra <mayank31398@gmail.com>
2024-07-08 23:34:05 -0400

    train_step_context

--- dolomite_engine/pretrain.py
+++ dolomite_engine/pretrain.py
@@ -122,19 +122,20 @@ def train(
     )
 
     train_step_context = nullcontext()
-    if args.distributed_args.use_dtensors_for_computation:
-        assert not use_nvte_fp8
-        train_step_context = enable_dtensors_for_computation()
-    elif use_nvte_fp8:
-        train_step_context = te.fp8_autocast(
-            enabled=True,
-            fp8_recipe=DelayedScaling(fp8_format=Format.HYBRID, amax_history_len=16, amax_compute_algo="max"),
-        )
 
     global_step = starting_iteration
     while global_step < num_training_steps:
         global_step += 1
         steps_since_start_time += 1
+
+        if args.distributed_args.use_dtensors_for_computation:
+            assert not use_nvte_fp8
+            train_step_context = enable_dtensors_for_computation()
+        elif use_nvte_fp8:
+            train_step_context = te.fp8_autocast(
+                enabled=True,
+                fp8_recipe=DelayedScaling(fp8_format=Format.HYBRID, amax_history_len=16, amax_compute_algo="max"),
+            )
 
         loss_step, grad_norm_step = train_step(
             model=model,
